99404ab7b57a7fc5345203a27f53ebf07eeb4301
hjink11 <hjink11@knou.ac.kr>
2024-11-14 01:56:18 +0900

    24/11/13

--- 04_jQuery/month.js
+++
@@ -1,8 +0,0 @@
-$('#formm').submit(function(e){
-    e.preventDefault()
-    let data = $('input[name="date"]').val()
-    let content = $('input[name="content"]').val()
-    alert(data)
-
-
-})
---
+++ 05_bootstrap/prac3.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    effect: "fade",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay:{
+        delay : 4000,
+        disableOnInteraction : false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      loop:true,
+
+  });
