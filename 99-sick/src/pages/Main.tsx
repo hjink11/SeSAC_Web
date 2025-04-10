a345ced260cc4acedb53be51ab171d89cd56288e
hjink11 <hjink11@knou.ac.kr>
2025-04-10 14:41:19 +0900

    tocken test

--- 99-sick/src/pages/Main.tsx
+++ 99-sick/src/pages/Main.tsx
@@ -4,7 +4,7 @@ export default function Main() {
   return (
     <>
       <Link to={"/test"} className="block">
-        테스트
+        테스트~~~!!
       </Link>
       <Link to={"/test2"}>테스트2</Link>
     </>
